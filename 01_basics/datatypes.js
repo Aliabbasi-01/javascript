0393aaef21fc78a1665e3b076abaa154e45a1064
Muhammad Ali <104286224+Aliabbasi-01@users.noreply.github.com>
2026-08-18 13:14:37 +0500

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,10 @@
+"use strict"// now all code is written as newer javascript for
+//alert( 3+3)  //currently we are using node js not browser  so its not working
+
+
+
+// node 01_basics/datatypes.js
+console.log("Ali");
+console.log(3+3)
+
+
