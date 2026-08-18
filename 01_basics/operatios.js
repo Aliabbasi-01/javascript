6b53464bdfede92d9836f87174ceb7dcf782a051
Muhammad Ali <104286224+Aliabbasi-01@users.noreply.github.com>
2026-08-18 14:07:23 +0500

    updated

---
+++ 01_basics/operatios.js
@@ -0,0 +1,17 @@
+let score="33"
+console.log(typeof score)
+let converToNumber=Number(score)
+console.log(typeof converToNumber)
+
+
+let IsLogedIn=1
+console.log(typeof IsLogedIn)
+let IsLoggedInBolean=Boolean(IsLogedIn)
+console.log( typeof IsLoggedInBolean);
+
+let name="Ali"
+console.log(typeof name);
+
+let convertname=Number(name)
+console.log(typeof convertname)
+console.log(convertname)
