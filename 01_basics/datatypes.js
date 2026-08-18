8849a05cce5e0620641e51c1047ae8851dcf7ec6
Muhammad Ali <104286224+Aliabbasi-01@users.noreply.github.com>
2026-08-18 14:08:15 +0500

    updated

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -20,23 +20,7 @@ console.log(typeof null);//type of null obbject
 console.log(typeof undefined);;//type of undefined is undefined
 
 
-let score="33"
-console.log(typeof score)
-let converToNumber=Number(score)
-console.log(typeof converToNumber)
 
-
-let IsLogedIn=1
-console.log(typeof IsLogedIn)
-let IsLoggedInBolean=Boolean(IsLogedIn)
-console.log( typeof IsLoggedInBolean);
-
-let name="Ali"
-console.log(typeof name);
-
-let convertname=Number(name)
-console.log(typeof convertname)
-console.log(convertname)
 
 
 
