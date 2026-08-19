7afb0b032c96b65d85a996983d36cced8aada1b9
Muhammad Ali <104286224+Aliabbasi-01@users.noreply.github.com>
2026-08-19 16:33:38 +0500

    updated code

---
+++ 01_basics/dates.js
@@ -0,0 +1,14 @@
+let mydate= new Date()
+
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleDateString())
+// console.log(mydate.toLocaleString())
+// console.log(typeof mydate)
+
+
+// let mycreateddate=new Date(2025,1,17)
+let mycreateddate=new Date(2025,1,17,17,30)
+console.log(mycreateddate.toDateString())
+console.log(mycreateddate.toLocaleString())
--- 01_basics/num_and_maths.js
+++ 01_basics/num_and_maths.js
@@ -1,4 +1,40 @@
-const score=400
-const newscore=new Number(600)
-console.log(score)
-console.log(newscore)
+// const score=400
+// const newscore=new Number(600)
+// console.log(score)
+// console.log(newscore)
+// console.log(newscore.toString().charAt(0))
+// console.log(newscore.toString().indexOf(6))
+
+// const result= new Number(345.678)
+// console.log(result.toString().split('.'))
+// console.log(result.toFixed(1))
+
+// console.log(result.toPrecision(3))
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString());
+
+
+//++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++++
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10) //is ma ab value 0 b a skti ha to is ko hm modify krty
+// console.log((Math.random() * 10)+1)  //decimal k bad vali values a ri han to inko asa handle krn ga 
+// console.log(Math.floor((Math.random() * 10)+1))  
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min +1)+min))   //random max or min k beach ma
+//or floor k use single value lana k lya
+
+
+
+
+
